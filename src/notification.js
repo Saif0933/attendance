7d725cd72b35da0c8befbb6ece603a608c531050
Ssaif3 <samsaif933@gmail.com>
2026-04-18 09:47:09 +0530

    changing-api-file

--- src/notification.js
+++ src/notification.js
@@ -1,9 +1,6 @@
 import messaging from '@react-native-firebase/messaging';
 import { Alert, PermissionsAndroid, Platform } from 'react-native';
 
-/**
- * Request Notification Permissions (Handles Android 13+ and iOS)
- */
 export async function requestUserPermission() {
   if (Platform.OS === 'android' && Platform.Version >= 33) {
     const granted = await PermissionsAndroid.request(
@@ -38,11 +35,9 @@ export const GetFCMToken = async () => {
   }
 };
 
-/**
- * Listener for Foreground Notifications
- */
+
 export const NotificationListener = () => {
-  // Listen for messages while the app is in the foreground
+  
   const unsubscribe = messaging().onMessage(async (remoteMessage) => {
     console.log('Foreground Message received:', JSON.stringify(remoteMessage));
     Alert.alert(
@@ -51,7 +46,7 @@ export const NotificationListener = () => {
     );
   });
 
-  // Listen for background messages when the app is opened from a notification
+  
   messaging().onNotificationOpenedApp((remoteMessage) => {
     console.log(
       'Notification caused app to open from background state:',
@@ -59,7 +54,7 @@ export const NotificationListener = () => {
     );
   });
 
-  // Check if the app was opened from a notification when it was totally closed (Killed state)
+  
   messaging()
     .getInitialNotification()
     .then((remoteMessage) => {
